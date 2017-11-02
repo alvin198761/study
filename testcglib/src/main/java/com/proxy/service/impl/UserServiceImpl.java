640ba6eacebf992a491efd3383fdc6695b4c0afd
gzz2017gzz <gzz_gzz@163.com>
2017-11-02 18:52:51 +0800

    asdasd
    asdasdasdasd

---
+++ testcglib/src/main/java/com/proxy/service/impl/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.proxy.service.impl;
+
+import com.proxy.service.UserService;
+
+public class UserServiceImpl implements UserService {
+	@Override
+	public String getName(int id) {
+		System.out.println("------getName------");
+		return "Tom";
+	}
+
+	@Override
+	public Integer getAge(int id) {
+		System.out.println("------getAge------");
+		return 10;
+	}
+}
